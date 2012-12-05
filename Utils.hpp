dd444a234a2bc211cce77330d3037a143caefa01
D.P. James <dj@deltadev.co.uk>
2012-12-05 11:58:58 +0000

    Formattng?

--- Utils.hpp
+++ Utils.hpp
@@ -9,82 +9,82 @@
 #include <unordered_set>
 
 namespace dpj_utils {
-
-template <typename T> struct printPair
-{ void operator()(T t) { std::cout << t.first << ' ' << t.second << '\n'; } };
-
-template <typename VectorType>
-void printVector(const VectorType& pload, size_t n = 10, std::ostream& os = std::cout)
-{
-    typedef typename VectorType::value_type value_type;
-    n = std::min(n, pload.size());
-
-    if (n > 10) 
+    
+    template <typename T> struct printPair
+    { void operator()(T t) { std::cout << t.first << ' ' << t.second << '\n'; } };
+    
+    template <typename VectorType>
+    void printVector(const VectorType& pload, size_t n = 10, std::ostream& os = std::cout)
     {
-        //
-        // Print an abridged version.
-        // 
-        std::copy(pload.begin(), pload.begin() + n / 2,
-                  std::ostream_iterator<value_type >(os, "\n"));
+        typedef typename VectorType::value_type value_type;
+        n = std::min(n, pload.size());
         
-        std::cout << "\n...\n...\n...\n\n" << std::endl;   
-
-        std::copy(pload.rbegin(), pload.rbegin() + n,
-                  std::ostream_iterator<value_type >(os, "\n"));
-        os << std::endl;
+        if (n > 10)
+        {
+            //
+            // Print an abridged version.
+            //
+            std::copy(pload.begin(), pload.begin() + n / 2,
+                      std::ostream_iterator<value_type >(os, "\n"));
+            
+            std::cout << "\n...\n...\n...\n\n" << std::endl;
+            
+            std::copy(pload.rbegin(), pload.rbegin() + n,
+                      std::ostream_iterator<value_type >(os, "\n"));
+            os << std::endl;
+        }
+        else
+        {
+            std::copy(pload.begin(), pload.end(), std::ostream_iterator<value_type >(os, "\n"));
+        }
     }
-    else
+    //
+    // order - obtain a rank or order of a container.
+    //
+    //     If the elements in the container are 11, 14, 12, 15, 13, then the order
+    //     is the sequence: 0 2 4 1 3 and the rank is 0 3 1 4 2. The rank is
+    //     obtained as the order of the order.
+    //     The order tells elements where to go, the rank tells them where they are!
+    //
+    // constructor
+    //     pass the container for which the order is wanted.
+    //
+    // index
+    //     returns a ref to the ordering.
+    
+    template <typename Container>
+    class Order
     {
-        std::copy(pload.begin(), pload.end(), std::ostream_iterator<value_type >(os, "\n"));
-    }
-}
-//
-// order - obtain a rank or order of a container.
-//
-//     If the elements in the container are 11, 14, 12, 15, 13, then the order
-//     is the sequence: 0 2 4 1 3 and the rank is 0 3 1 4 2. The rank is
-//     obtained as the order of the order.
-//     The order tells elements where to go, the rank tells them where they are!
-// 
-// constructor
-//     pass the container for which the order is wanted.
-//
-// index
-//     returns a ref to the ordering.
-
-template <typename Container>
-class Order
-{
-  public:
-    typedef typename Container::value_type value_type;
-    typedef std::pair<value_type, unsigned> Pair;
-
-    Order(Container const& c)
+    public:
+        typedef typename Container::value_type value_type;
+        typedef std::pair<value_type, unsigned> Pair;
+        
+        Order(Container const& c)
         : m_begin(std::begin(c)), m_end(std::end(c)) { setup(); }
-
-    std::vector<unsigned> const& index() { return m_index; }
-
-  private:    
-    void setup()
-    {
-        auto it = m_begin;
-        for (unsigned i = 0; it != m_end; ++it, ++i) 
+        
+        std::vector<unsigned> const& index() { return m_index; }
+        
+    private:
+        void setup()
         {
-            m_ps.push_back(Pair(*it, i));
+            auto it = m_begin;
+            for (unsigned i = 0; it != m_end; ++it, ++i)
+            {
+                m_ps.push_back(Pair(*it, i));
+            }
+            std::sort(m_ps.begin(), m_ps.end());
+            m_index.reserve(std::distance(m_begin, m_end));
+            for (auto const& p : m_ps)
+            {
+                m_index.push_back(std::get<1>(p));
+            }
         }
-        std::sort(m_ps.begin(), m_ps.end());
-        m_index.reserve(std::distance(m_begin, m_end));
-        for (auto const& p : m_ps)
-        {
-            m_index.push_back(std::get<1>(p));
-        }
-    }
-    typename Container::const_iterator m_begin;
-    typename Container::const_iterator m_end;
-    std::vector<Pair> m_ps;
-    std::vector<unsigned> m_index;
-};
-
+        typename Container::const_iterator m_begin;
+        typename Container::const_iterator m_end;
+        std::vector<Pair> m_ps;
+        std::vector<unsigned> m_index;
+    };
+    
     struct IsNaNPredicate
     {
         template <typename T>
@@ -115,10 +115,10 @@ class Order
                 std::cerr << "Error: trying to build histogram with nodata!. Exiting...\n";
                 exit(EXIT_FAILURE);
             }
-
+            
             // Remove any NaNs
             m_data.erase(std::remove_if(m_data.begin(), m_data.end(), IsNaNPredicate()), m_data.end());
-
+            
             std::sort(m_data.begin(), m_data.end());
             m_min = m_data.front();
             m_max = m_data.back();
@@ -126,13 +126,13 @@ class Order
             
             // This makes sure the final bin captures the largest value after rounding errors.
             // It is also a dumb way to do it. Sorry.
-            while (m_min + m_binWidth * m_numBins <= m_max) 
+            while (m_min + m_binWidth * m_numBins <= m_max)
             {
                 m_binWidth += std::numeric_limits<T>::epsilon() * m_numBins;
             }
             
             m_rightHandEndPoints.push_back(m_min + m_binWidth);
-            for (unsigned i = 1; i < m_numBins; ++i) 
+            for (unsigned i = 1; i < m_numBins; ++i)
             {
                 m_rightHandEndPoints.push_back(m_rightHandEndPoints.back() + m_binWidth);
             }
@@ -141,12 +141,12 @@ class Order
             //
             auto it = m_data.begin();
             auto end = m_data.end();
-            for (auto a : m_rightHandEndPoints) 
+            for (auto a : m_rightHandEndPoints)
             {
                 auto tmp = std::upper_bound(it, end, a);
                 m_counts.push_back((unsigned)std::distance(it, tmp));
                 it = tmp;
-            }        
+            }
         }
         unsigned maxCount() { return *std::max_element(m_counts.begin(), m_counts.end());}
         
@@ -156,21 +156,24 @@ class Order
         
         T max() { return m_max; }
         T min() { return m_min; }
-
+        
         void summary(std::ostream& os = std::cout)
+        {
+            os << "min value: " << m_min << '\n';
+            os << "max value: " << m_max << '\n';
+            os << "num bins : " << m_numBins << '\n';
+            os << "bin width: " << m_binWidth << '\n';
+            os << "rightHandEndpoint count\n";
+            for (unsigned i = 0; i < m_rightHandEndPoints.size(); ++i)
             {
-                os << "min value: " << m_min << '\n';
-                os << "max value: " << m_max << '\n';
-                os << "num bins : " << m_numBins << '\n';
-                os << "bin width: " << m_binWidth << '\n';
-                os << "rightHandEndpoint count\n";
-                for (unsigned i = 0; i < m_rightHandEndPoints.size(); ++i)
-                {
-                    os << m_rightHandEndPoints[i] << ' ' << m_counts[i] << '\n';
-                }
+                os << m_rightHandEndPoints[i] << ' ' << m_counts[i] << '\n';
             }
+        }
     };
+    
+    
 
+    
 } // namespace dpj_utils
 
 #endif //Utils_hpp

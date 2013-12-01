6ede7209d7e88e40d6b5120a09b9d8cf45885c50
D.P. James <dj@deltadev.co.uk>
2013-12-01 12:37:55 +0000

    Some network tools in net namespace.

--- dpj_utils.hh
+++ dpj_utils.hh
@@ -149,4 +149,30 @@ namespace dpj {
   }
 }
 
+namespace net {
+
+  /// gets a string network address from a sockaddr.
+  std::string ntop(int family, sockaddr* addr) {
+    std::vector<char> p;
+    switch (family) {
+      case AF_INET:
+        p.resize(INET_ADDRSTRLEN);
+        inet_ntop(family, &((sockaddr_in*)addr)->sin_addr, p.data(), (socklen_t)p.size());
+        break;
+      case AF_INET6:
+        p.resize(INET6_ADDRSTRLEN);
+        inet_ntop(family, &((sockaddr_in6*)addr)->sin6_addr, p.data(), (socklen_t)p.size());
+        break;
+      default:
+        return "Problem converting to network address.";
+    }
+    return p.data();
+  }
+
+  std::string ntop(int family, sockaddr_storage* addr) {
+    return ntop(family, (sockaddr*)addr);
+  }
+}
+
+
 #endif /* _DPJ_UTILS_H_ */

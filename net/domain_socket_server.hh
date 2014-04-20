48ca20d036b71a4392b04f9b7ce6270659708118
D.P. James <dj@deltadev.co.uk>
2014-04-20 19:22:30 +0100

    Work on net::local server & session.

--- net/domain_socket_server.hh
+++ net/domain_socket_server.hh
@@ -6,61 +6,46 @@
 #include <memory>
 #include <functional>
 
-using namespace std::placeholders;
-using std::bind;
-
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
 namespace dpj { namespace net { namespace local {
+
+  using namespace std::placeholders;
+  using std::bind;
+  
+  using boost::system::error_code;
   
   namespace asio = boost::asio;
   using asio::local::stream_protocol;
+  using asio::buffer;
+  using asio::async_write;
+
   
   class session : public std::enable_shared_from_this<session>
   {
   public:
-    session(boost::asio::io_service& io_service)
-    : socket_(io_service)
-    {
-    }
+    session(boost::asio::io_service& io_service) : socket_{io_service} { }
     
-    stream_protocol::socket& socket()
-    {
-      return socket_;
-    }
+    stream_protocol::socket& socket() { return socket_; }
     
     void start()
-    {
-      socket_.async_read_some(asio::buffer(data_),
-                              bind(&session::handle_read, shared_from_this(), _1, _2));
-    }
+    { socket_.async_read_some(buffer(data_), bind(&session::read, shared_from_this(), _1, _2)); }
     
-    void handle_read(const boost::system::error_code& error,
-                     size_t bytes_transferred)
-    {
-      std::cout << std::string{data_.data(), bytes_transferred} << '\n';
-      if (!error)
-      {
-        asio::async_write(socket_, asio::buffer(data_, bytes_transferred),
-                          bind(&session::handle_write, shared_from_this(), _1));
-      }
-    }
-    
-    void handle_write(const boost::system::error_code& error)
+    void read(error_code const& error, size_t n)
     {
       if (!error)
-      {
-        socket_.async_read_some(asio::buffer(data_),
-                                std::bind(&session::handle_read, shared_from_this(), _1, _2));
-      }
+        async_write(socket_, buffer(data_, n), bind(&session::write, shared_from_this(), _1));
+    }
+    
+    void write(error_code const& error)
+    {
+      if (!error)
+        socket_.async_read_some(buffer(data_), bind(&session::read, shared_from_this(), _1, _2));
     }
     
   private:
-    // The socket used to communicate with the client.
     stream_protocol::socket socket_;
-    
-    // Buffer used to store data received from the client.
     std::array<char, 1024> data_;
   };
   
@@ -68,24 +53,20 @@ namespace dpj { namespace net { namespace local {
   {
   public:
     server(boost::asio::io_service& io_service, std::string file)
-    : io_service_(io_service),
-    
-    acceptor_(io_service, stream_protocol::endpoint(file))
+    : io_service_(io_service)
+    , acceptor_(io_service, stream_protocol::endpoint(file))
     {
-      auto new_session = std::make_shared<session>(io_service_);
-      acceptor_.async_accept(new_session->socket(), bind(&server::handle_accept, this, new_session, _1));
+      auto s = std::make_shared<session>(io_service_);
+      acceptor_.async_accept(s->socket(), bind(&server::handle_accept, this, s, _1));
     }
     
-    void handle_accept(std::shared_ptr<session> new_session,
-                       const boost::system::error_code& error)
+    void handle_accept(std::shared_ptr<session> s, error_code const& error)
     {
       if (!error)
-      {
-        new_session->start();
-      }
+        s->start();
       
-      new_session.reset(new session(io_service_));
-      acceptor_.async_accept(new_session->socket(), bind(&server::handle_accept, this, new_session, _1));
+      s.reset(new session{io_service_});
+      acceptor_.async_accept(s->socket(), bind(&server::handle_accept, this, s, _1));
     }
     
   private:
